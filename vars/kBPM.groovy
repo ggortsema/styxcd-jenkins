44ae2212e378c87edfe98cefc104047cebf55f0a
Grant Gortsema <grant.gortsema@gmail.com>
2026-04-20 17:16:18 -0400

    Adding in body

--- vars/kBPM.groovy
+++ vars/kBPM.groovy
@@ -10,4 +10,10 @@ def call(body) {
         echo "Reading configuration 2"
 
     }
+
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    echo "IN KBPM with ${pipelineParams}"
 }
