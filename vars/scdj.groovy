a81f7d5f7bf83e557c8098c22631608d7ca03a3e
Grant Gortsema <grant.gortsema@gmail.com>
2026-04-20 18:13:10 -0400

    parsing yml

--- vars/scdj.groovy
+++ vars/scdj.groovy
@@ -14,4 +14,13 @@ def call(body) {
     body()
 
     echo "IN scdj with ${pipelineParams}"
+
+    if(pipelineParams.yml) {
+        yml_string = pipelineParams.yml
+        echo "HERE IS THE YML STRING WE ARE GETTING:"
+        echo yml_string
+        yml = readYaml text: yml_string
+
+        echo "current workflow is: " + yml.worflow
+    }
 }
