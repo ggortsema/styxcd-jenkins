3e4733574a1e772381805e9c90bb22d5c81105db
Grant Gortsema <grant.gortsema@gmail.com>
2026-04-20 20:42:00 -0400

    adding in init and cleanup

--- src/org/styxcd/pipeline/workflow/DummyWorkflow.groovy
+++ src/org/styxcd/pipeline/workflow/DummyWorkflow.groovy
@@ -8,7 +8,9 @@ def createJsonStageList(yml, getStage) {
     def paramMap = [:]
     paramMap['VALIDATE_MAP'] = preprocessYml(yml)
 
+    jsonOutput['DummyWorkflowInitialize'] = getStage['DummyWorkflowInitialize'].getParams(yml, paramMap)
     jsonOutput['DummyWorkflowBody'] = getStage['DummyWorkflowBody'].getParams(yml, paramMap)
+    jsonOutput['DummyWorkflowCleanup@final'] = getStage['DummyWorkflowCleanup'].getParams(yml, paramMap)
 
     return jsonOutput
 }
