1eba06ef10743a88a37230608789f26811564c3e
Grant Gortsema <grant.gortsema@gmail.com>
2026-04-20 20:44:40 -0400

    defining init and cleanup in map

--- src/org/styxcd/pipeline/stages/StageMap.groovy
+++ src/org/styxcd/pipeline/stages/StageMap.groovy
@@ -4,6 +4,8 @@ def getMap(steps, featureFlags) {
     def map = [:]
 
     map["DummyWorkflowBody"] = new org.styxcd.pipeline.stages.stagesimpl.DummyWorkflowBody(steps, featureFlags)
+    map["DummyWorkflowInitialize"] = new org.styxcd.pipeline.stages.stagesimpl.DummyWorkflowInitialize(steps, featureFlags)
+    map["DummyWorkflowCleanup"] = new org.styxcd.pipeline.stages.stagesimpl.DummyWorkflowCleanup(steps, featureFlags)
 
     return map
 }
