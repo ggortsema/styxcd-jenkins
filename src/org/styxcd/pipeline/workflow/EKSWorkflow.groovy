64efe0fd1f8dd34fbe27a68460f9dcb688f2c7c2
Grant Gortsema <grant.gortsema@gmail.com>
2026-04-28 21:59:05 -0400

    putting the whole workflow together

--- src/org/styxcd/pipeline/workflow/EKSWorkflow.groovy
+++ src/org/styxcd/pipeline/workflow/EKSWorkflow.groovy
@@ -9,11 +9,11 @@ def createJsonStageList(yml, getStage) {
     paramMap['VALIDATE_MAP'] = preprocessYml(yml)
 
     jsonOutput['EKSWorkflowInitialize'] = getStage['EKSWorkflowInitialize'].getParams(yml, paramMap)
-    //jsonOutput['EKSWorkflowClusterTeardown'] = getStage['EKSWorkflowClusterTeardown'].getParams(yml, paramMap)
-    //jsonOutput['EKSWorkflowClusterBuild'] = getStage['EKSWorkflowClusterBuild'].getParams(yml, paramMap)
-    //jsonOutput['EKSWorkflowLoadBalancingController'] = getStage['EKSWorkflowLoadBalancingController'].getParams(yml, paramMap)
-    //jsonOutput['EKSWorkflowDeployImages'] = getStage['EKSWorkflowDeployImages'].getParams(yml, paramMap)
+    jsonOutput['EKSWorkflowClusterTeardown'] = getStage['EKSWorkflowClusterTeardown'].getParams(yml, paramMap)
+    jsonOutput['EKSWorkflowClusterBuild'] = getStage['EKSWorkflowClusterBuild'].getParams(yml, paramMap)
+    jsonOutput['EKSWorkflowLoadBalancingController'] = getStage['EKSWorkflowLoadBalancingController'].getParams(yml, paramMap)
     jsonOutput['EKSWorkflowBuildImages'] = getStage['EKSWorkflowBuildImages'].getParams(yml, paramMap)
+    jsonOutput['EKSWorkflowDeployImages'] = getStage['EKSWorkflowDeployImages'].getParams(yml, paramMap)
     jsonOutput['EKSWorkflowCleanup@final'] = getStage['EKSWorkflowCleanup'].getParams(yml, paramMap)
 
 
