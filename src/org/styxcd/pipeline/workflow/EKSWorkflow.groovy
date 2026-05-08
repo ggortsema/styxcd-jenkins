b1263c29077a115b507d96df14d10f65a7b8e6fa
Grant Gortsema <grant.gortsema@gmail.com>
2026-05-08 19:17:37 -0400

    adding build env, teardown ev, and deploy apps to eks workflow

--- src/org/styxcd/pipeline/workflow/EKSWorkflow.groovy
+++ src/org/styxcd/pipeline/workflow/EKSWorkflow.groovy
@@ -8,12 +8,23 @@ def createJsonStageList(yml, getStage) {
     def paramMap = [:]
     paramMap['VALIDATE_MAP'] = preprocessYml(yml)
 
+
     jsonOutput['EKSWorkflowInitialize'] = getStage['EKSWorkflowInitialize'].getParams(yml, paramMap)
-    jsonOutput['EKSWorkflowClusterTeardown'] = getStage['EKSWorkflowClusterTeardown'].getParams(yml, paramMap)
-    jsonOutput['EKSWorkflowClusterBuild'] = getStage['EKSWorkflowClusterBuild'].getParams(yml, paramMap)
-    jsonOutput['EKSWorkflowLoadBalancingController'] = getStage['EKSWorkflowLoadBalancingController'].getParams(yml, paramMap)
-    jsonOutput['EKSWorkflowBuildImages'] = getStage['EKSWorkflowBuildImages'].getParams(yml, paramMap)
-    jsonOutput['EKSWorkflowDeployImages'] = getStage['EKSWorkflowDeployImages'].getParams(yml, paramMap)
+
+    if(yml?.teardown_env) {
+        jsonOutput['EKSWorkflowClusterTeardown'] = getStage['EKSWorkflowClusterTeardown'].getParams(yml, paramMap)
+    }
+    if(yml?.build_env) {
+        jsonOutput['EKSWorkflowClusterBuild'] = getStage['EKSWorkflowClusterBuild'].getParams(yml, paramMap)
+        jsonOutput['EKSWorkflowLoadBalancingController'] = getStage['EKSWorkflowLoadBalancingController'].getParams(yml, paramMap)
+    }
+    if(yml?.depploy_apps) {
+        jsonOutput['EKSWorkflowBuildImages'] = getStage['EKSWorkflowBuildImages'].getParams(yml, paramMap)
+        jsonOutput['EKSWorkflowDeployImages'] = getStage['EKSWorkflowDeployImages'].getParams(yml, paramMap)
+    }
+
+
+
     jsonOutput['EKSWorkflowCleanup@final'] = getStage['EKSWorkflowCleanup'].getParams(yml, paramMap)
 
 
