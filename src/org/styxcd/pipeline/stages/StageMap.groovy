9b5998856c5173d2a475b41ccce877d3e4009d1a
Grant Gortsema <grant.gortsema@gmail.com>
2026-06-09 20:32:50 -0400

    changing stage registry names

--- src/org/styxcd/pipeline/stages/StageMap.groovy
+++ src/org/styxcd/pipeline/stages/StageMap.groovy
@@ -3,9 +3,9 @@ package org.styxcd.pipeline.stages
 def getMap(steps, featureFlags) {
     def map = [:]
 
-    map["cloud_workflow:initialize"] = { new org.styxcd.pipeline.stages.stagesimpl.CloudWorkflowInitialize(steps, featureFlags) }
-    map["cloud_workflow:gradle_build"] = { new org.styxcd.pipeline.stages.stagesimpl.GradleBuild(steps, featureFlags) }
-    map["cloud_workflow:cleanup"] = { new org.styxcd.pipeline.stages.stagesimpl.CloudWorkflowCleanup(steps, featureFlags) }
+    map["CloudWorkflowInitialize"] = { new org.styxcd.pipeline.stages.stagesimpl.CloudWorkflowInitialize(steps, featureFlags) }
+    map["GradleBuild"] = { new org.styxcd.pipeline.stages.stagesimpl.GradleBuild(steps, featureFlags) }
+    map["CloudWorkflowCleanup"] = { new org.styxcd.pipeline.stages.stagesimpl.CloudWorkflowCleanup(steps, featureFlags) }
 
     return map
 }
