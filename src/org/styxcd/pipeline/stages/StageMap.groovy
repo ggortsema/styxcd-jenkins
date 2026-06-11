059c2f0754d3dc2f801e4fc6a664706e540184f3
Grant Gortsema <grant.gortsema@gmail.com>
2026-06-10 21:07:15 -0400

    fixing cloudworkflow nullsafe traversal

--- src/org/styxcd/pipeline/stages/StageMap.groovy
+++ src/org/styxcd/pipeline/stages/StageMap.groovy
@@ -7,6 +7,6 @@ def getMap(steps, featureFlags) {
     map["GradleBuild"] = { new org.styxcd.pipeline.stages.stagesimpl.GradleBuild(steps, featureFlags) }
     map["CloudWorkflowCleanup"] = { new org.styxcd.pipeline.stages.stagesimpl.CloudWorkflowCleanup(steps, featureFlags) }
     map["GkeSandbox"] = { new org.styxcd.pipeline.stages.stagesimpl.GkeSandbox(steps, featureFlags) }
-    
+
     return map
 }
